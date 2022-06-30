f2e391f7edc7283d76020cab0570f5f7c5d53142
PARAGDOLHARE <paragbdolhare@gmail.com>
2022-06-30 22:47:13 +0530

    toast and name of location

--- app/src/main/java/com/example/feedback/location.java
+++ app/src/main/java/com/example/feedback/location.java
@@ -1,6 +1,7 @@
 package com.example.feedback;
 
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -10,33 +11,40 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-
-public class location extends AppCompatActivity {
-
-
+public class location extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         setContentView(R.layout.activity_location);
-        Button b1= findViewById(R.id.button);
+        Button b1 = findViewById(R.id.button);
         b1.setOnClickListener(new View.OnClickListener() {
+
+            @SuppressLint("Wrongconstant")
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(location.this,review.class);
+                Intent intent = new Intent(location.this, review.class);
 
-                EditText editText=(EditText) findViewById(R.id.editTextTextPersonName);
+                EditText editText1 = (EditText) findViewById(R.id.editTextlocation);
+                String next = editText1.getText().toString();
+                if (next.equals("toilet 1")||next.equals("toilet 2")||next.equals("toilet 3")||next.equals("TOILET 1")||next.equals("TOILET 2")||next.equals("TOILET 3")||next.equals("Toilet 1")||next.equals("Toilet 2")||next.equals("Toilet 3")){
+                    editText1.setVisibility(1);
 
-                String next =editText.getText().toString();
-                // String next =editText1.getText().toString();
-                intent.putExtra("Next",next);
-                startActivity(intent);
+                    intent.putExtra("Next", next);
+
+                    startActivity(intent);
+
+                }
+                else {
+                    Toast.makeText(location.this, "Location can't be empty", Toast.LENGTH_SHORT).show();
+                    editText1.setVisibility(0);
+                }
+
+
             }
         });
-    }}
+    }
+}
 
 
--- app/src/main/java/com/example/feedback/review.java
+++ app/src/main/java/com/example/feedback/review.java
@@ -10,5 +10,7 @@ public class review extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
+
+
     }
 }
